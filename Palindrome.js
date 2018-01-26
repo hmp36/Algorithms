031519f0d352cea4eb2df44d0b099f8cff9a35de
Hagan <haganpratt@gmail.com>
2018-01-25 19:25:15 -0500

    a couple algos

---
+++ Palindrome.js
@@ -0,0 +1,13 @@
+function is Palindrome (word);{
+    var x = word;
+    var y = word.length-1;
+    word = dad;
+    for(var i=0;i < wor.length/2;i++){
+        if(x[i]==x[y]){
+        }
+        else{
+            return false;
+        }
+    }
+    return true;
+}
---
+++ reverse_array.js
@@ -0,0 +1,8 @@
+var arr = [0,4,3,1,Apple]
+math.floor
+for (vari=0;i<(arr.length/2);i++){
+    temp=arr[i];
+arr[i]=arr[(arr.length-1)-i] 
+       arr[(arr.length-1)-i] = temp;
+}  
+return arr; 
