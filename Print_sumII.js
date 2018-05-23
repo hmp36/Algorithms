ab93c49579d3294fd095d6725ef760854c3e63b4
Hagan <haganpratt@gmail.com>
2018-05-23 09:58:12 -0400

    algos

---
+++ Print_sumII.js
@@ -0,0 +1,8 @@
+(Print Sum) all odd from 1 to 5000
+
+var sum = 0;
+for (var i=1; i<5000; i = i+2)
+{
+    sum = sum +i;
+}
+console.log(sum);
---
+++ iterating_through_an_array.js
@@ -0,0 +1,8 @@
+Iterating through the Array
+
+var x = [1,3,5,7,9,13];
+
+for (var i=0;i<x.length;i++)
+{
+    console.log (x[i]);
+}
