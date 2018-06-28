b4f02062fa99e8c2c4a17f3ae34bdcc8d29b1408
Hagan <haganpratt@gmail.com>
2018-06-27 23:51:19 -0400

    >>

---
+++ linearsearch.js
@@ -0,0 +1,17 @@
+function linearSearch (key,arr){
+var arr = [0,67,7,8,9,10];
+var key = 7
+    for(i=0;i < length(arr);i++){
+        if(array[i] == key){
+            return i
+        }
+    }
+}
+    return -1
+ 
+    console.log (i);
+
+
+
+
+    /////not correct yet!!!!
